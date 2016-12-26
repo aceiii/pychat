f317a70dae944d01304478a77bc58885f9ec486d
Borin Ouch <borin.ouch@gmail.com>
2016-12-26 09:14:47 -0800

    Add user names and avatars

--- main.py
+++ main.py
@@ -6,11 +6,44 @@ from __future__ import division
 from __future__ import print_function
 
 import socketio
-import eventlet
 from flask import Flask, render_template
+
+from random import randint
+from hashlib import md5
+
+GRAVATAR_URL = "http://www.gravatar.com/avatar/%s?s=64&d=identicon&r=PG"
 
 sio = socketio.Server()
 app = Flask(__name__)
+
+users = {}
+
+def get_value_or_default(obj, prop, default=None):
+    if prop in obj:
+        return obj[prop]
+    return default
+
+def iter_user_props(prop_name):
+    return (get_value_or_default(u, prop_name) for u in users.itervalues())
+
+def user_prop(sid, prop_name):
+    return get_value_or_default(users[sid], prop_name)
+
+def random_username():
+    return "Guest" + str(randint(2**16, 2**32))
+
+def is_unique_username(name):
+    return not any(n == name for n in iter_user_props("name"))
+
+def unique_random_username():
+    while True:
+        new_name = random_username()
+        if is_unique_username(new_name):
+            return new_name
+
+def gravatar_for_username(username):
+    name_hash = md5(username).hexdigest()
+    return GRAVATAR_URL % (name_hash,)
 
 @app.route("/")
 def index():
@@ -18,18 +51,44 @@ def index():
 
 @sio.on("connect")
 def connect(sid, env):
-    print("connect", sid, env)
+    print("connected:", sid)
+    users[sid] = {"env": env}
 
 @sio.on("chat message")
 def message(sid, data):
     print("message", sid, data)
-    sio.emit("chat message", "%s:%s" % (sid, data))
+    context = {
+        "img": user_prop(sid, "avatar"),
+        "username": user_prop(sid, "name"),
+        "message": data
+    }
+    sio.emit("chat message", context)
 
 @sio.on("disconnect")
 def disconnect(sid):
-    print("disconnect", sid)
+    print("User disconnected. Clearing data for user", sid)
+    del users[sid]
+
+@sio.on("client request_name")
+def change_username(sid, new_name):
+    if new_name and not is_unique_username(new_name):
+        return
+
+    new_avatar = None
+    if not new_name:
+        new_name = unique_random_username()
+        new_avatar = gravatar_for_username(new_name)
+
+    print("Setting name for", sid, "to", new_name)
+    users[sid]["name"] = new_name
+    if new_avatar:
+        users[sid]["avatar"] = new_avatar
+    sio.emit("server update_name", new_name, sid)
+
+flask_app = app
+app = socketio.Middleware(sio, flask_app)
 
 if __name__ == "__main__":
-    app = socketio.Middleware(sio, app)
+    import eventlet
     eventlet.wsgi.server(eventlet.listen(("", 3000)), app)
 
--- static/main.js
+++ static/main.js
@@ -1,11 +1,50 @@
 
+var $messages = $("#messages");
+var $input = $("#input");
+var $form = $("form");
+
 var socket = io();
-$("form").submit(function () {
-    socket.emit("chat message", $("#input").val());
-    $("#input").val("");
+var user = {};
+
+var templates = {
+    chat_message: _.template($("#template-chat-message").html()),
+};
+
+function append_chat_message(context) {
+    $messages.append(templates.chat_message(context));
+}
+
+function append_server_message(context) {
+    $messages.append($("<li class='server-message'>").text(context.message));
+}
+
+$form.submit(function () {
+    socket.emit("chat message", $input.val());
+    $input.val("");
+    $messages[0].scrollTo(0, $messages[0].scrollHeight);
     return false;
 });
-socket.on("chat message", function(msg) {
-    $("#messages").append($("<li>").text(msg));
+
+socket.on("connect", function() {
+    console.log("Connected to chat server.");
+    append_server_message({ message: "Connected to server." });
+    var name = user.name || "";
+    socket.emit("client request_name", name);
+});
+
+socket.on("disconnect", function() {
+    console.log("Disconnected from chat server.");
+    append_server_message({ context: "Disconnected from server." });
+});
+
+socket.on("chat message", function(context) {
+    console.log(context);
+    append_chat_message(context);
+});
+
+socket.on("server update_name", function(name) {
+    console.log("Setting username to: ", name)
+    append_server_message({ message: "Setting username to: " + name });
+    user.name = name;
 });
 
